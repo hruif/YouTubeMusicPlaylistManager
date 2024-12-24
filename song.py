97ad1b260060e66db66093750f0ec271f76cd3df
hruif <hruif@hotmail.com>
2024-12-23 21:14:10 -0800

    song class

---
+++ song.py
@@ -0,0 +1,6 @@
+class Song:
+    def __init__(self, name: str, author: str, link: str, length: float):
+        self.name = name
+        self.author = author
+        self.link = link
+        self.length = length
