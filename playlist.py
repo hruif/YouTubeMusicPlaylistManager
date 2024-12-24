e4dbdc24d9528d06f8beccb32ae337697c821299
hruif <hruif@hotmail.com>
2024-12-23 21:10:16 -0800

    added functions to playlist.py

--- playlist.py
+++ playlist.py
@@ -1,24 +1,40 @@
 from thefuzz import process
-from song.py import Song
+from datetime import datetime
+from song import Song
+
+# --------------------------- NEED TESTING ---------------------------------------------------------------
 
 # Playlist
+sorting = "date"
 songs = []
 
 # Add song to playlist
-def addSong(song):
-    songs.add(song)
+def addSong(song: Song, time_added: float):
+    songs.add((song, time_added))
+    if (sorting == "date"):
+        sortByDate()
+    elif (sorting == "name"):
+        sortByName()
 
 # Remove song from playlist
-def removeSong(song):
-    songs.remove(song)
+def removeSong(song: Song):
+    songs = [pair for pair in songs if pair[0] != song]
 
 # Search within playlist
-def search(query):
+def search(query: str):
     results = process.extract(query, songs, limit=10)
     return results
 
 # Check if song is within the playlist
-def isInPlaylist(song):
+def isInPlaylist(song: Song):
     return (song in songs)
 
-def sortByDate(song):
+# Sort playlist by date added
+def sortByDate():
+    songs = sorted(songs, key=lambda pair: pair[1])
+    sorting = "date"
+
+# Sort playlist by name
+def sortByName():
+    songs = sorted(songs, key=lambda pair: pair[0].name)
+    sorting = "name"
